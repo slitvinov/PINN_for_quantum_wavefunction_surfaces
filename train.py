71770660a413d52e0b77d2319b23b69017415732
Sergey Litvinov <slitvinov@gmail.com>
2023-03-07 15:45:59 +0100

    up

--- train.py
+++ train.py
@@ -1,4 +1,3 @@
-import numpy as np
 import torch
 
 
@@ -8,8 +7,7 @@ class toR(torch.nn.Module):
 	def forward(input):
 		r2 = input[:, 0].pow(2) + input[:, 1].pow(2) + input[:, 2].pow(2)
 		r = torch.sqrt(r2)
-		r = r.reshape(-1, 1)
-		return r
+		return r.reshape(-1, 1)
 
 
 class atomicAct_s(torch.nn.Module):
@@ -28,18 +26,11 @@ def d2fx(x, f):
 	                           create_graph=True)[0]
 
 
-def radial(x, y, z, R):
+def hamiltonian(x, y, z, R, psi):
 	r1 = torch.sqrt((x - R)**2 + y**2 + z**2)
 	r2 = torch.sqrt((x + R)**2 + y**2 + z**2)
-	return r1, r2
-
-
-def V(x, y, z, R):
-	r1, r2 = radial(x, y, z, R)
-	return -1 / r1 - 1 / r2
-
-def hamiltonian(x, y, z, R, psi):
-	return -0.5 * (d2fx(x, psi) + d2fx(y, psi) + d2fx(z, psi)) + V(x, y, z, R) * psi
+	return -0.5 * (d2fx(x, psi) + d2fx(y, psi) + d2fx(z, psi)) - (1 / r1 +
+	                                                              1 / r2) * psi
 
 
 def sampling(n_points):
@@ -51,8 +42,6 @@ def sampling(n_points):
 	r2 = (x + R)**2 + y**2 + z**2
 	x[r1 < cutOff**2] = cutOff
 	x[r2 < cutOff**2] = cutOff
-	x, y, z = x.reshape(-1, 1), y.reshape(-1, 1), z.reshape(-1, 1)
-	R = R.reshape(-1, 1)
 	x.requires_grad = True
 	y.requires_grad = True
 	z.requires_grad = True
@@ -80,24 +69,6 @@ class NN_ion(torch.nn.Module):
 		self.netDecayL = torch.nn.Linear(1, netDecay_neurons, bias=True)
 		self.netDecay = torch.nn.Linear(netDecay_neurons, 1, bias=True)
 
-	def forward(self, x, y, z, R):
-		e = self.Lin_E1(R)
-		e = self.sig(e)
-		e = self.Lin_E2(e)
-		e = self.sig(e)
-		E = self.Lin_Eout(e)
-		fi_r1, fi_r2 = self.atomicUnit(x, y, z, R)
-		fi_r1m, fi_r2m = self.atomicUnit(-x, y, z, R)
-		N_LCAO = self.lcao_solution(fi_r1, fi_r2)
-		B = self.base(fi_r1, fi_r2) + self.base(fi_r1m, fi_r2m)
-		NN = self.Lin_out(B)
-		f = self.netDecayL(R)
-		f = self.sig(f)
-		f = self.netDecay(f)
-		NN = NN * f
-		Nout = NN + N_LCAO
-		return Nout, E
-
 	def atomicUnit(self, x, y, z, R):
 		rVec1 = torch.cat((x - R, y, z), 1)
 		r1 = self.toR(rVec1)
@@ -107,20 +78,25 @@ class NN_ion(torch.nn.Module):
 		fi_r2 = self.actAO_s(r2)
 		return fi_r1, fi_r2
 
-	def lcao_solution(self, fi_r1, fi_r2):
-		N_LCAO = (fi_r1 + fi_r2)
-		return N_LCAO
-
 	def base(self, fi_r1, fi_r2):
 		fi_r = torch.cat((fi_r1, fi_r2), 1)
 		fi_r = self.Lin_H1(fi_r)
 		fi_r = self.sig(fi_r)
 		fi_r = self.Lin_H2(fi_r)
-		fi_r = self.sig(fi_r)
-		return fi_r
+		return self.sig(fi_r)
 
 	def LossFunctions(self, x, y, z, R, bIndex1, bIndex2):
-		psi, E = self.forward(x, y, z, R)
+		e = self.Lin_E1(R)
+		e = self.sig(e)
+		e = self.Lin_E2(e)
+		e = self.sig(e)
+		fi_r1, fi_r2 = self.atomicUnit(x, y, z, R)
+		fi_r1m, fi_r2m = self.atomicUnit(-x, y, z, R)
+		B = self.base(fi_r1, fi_r2) + self.base(fi_r1m, fi_r2m)
+		f = self.netDecayL(R)
+		f = self.sig(f)
+		psi = self.Lin_out(B) * self.netDecay(f) + fi_r1 + fi_r2
+		E = self.Lin_Eout(e)
 		res = hamiltonian(x, y, z, R, psi) - E * psi
 		LossPDE = (res.pow(2)).mean()
 		Lbc = (psi[bIndex1].pow(2)).mean() + (psi[bIndex2].pow(2)).mean()
@@ -136,9 +112,10 @@ def train():
 		optimizer.zero_grad()
 		if tt % sc_sampling == 0 and tt < 0.9 * epochs:
 			x, y, z, R = sampling(n_train)
-			r1, r2 = radial(x, y, z, R)
-			bIndex1 = torch.where(r1 >= BCcutoff)
-			bIndex2 = torch.where(r2 >= BCcutoff)
+			r1 = (x - R)**2 + y**2 + z**2
+			r2 = (x + R)**2 + y**2 + z**2
+			bIndex1 = torch.where(r1 >= BCcutoff**2)
+			bIndex2 = torch.where(r2 >= BCcutoff**2)
 		Ltot = model.LossFunctions(x, y, z, R, bIndex1, bIndex2)
 		Ltot.backward(retain_graph=False)
 		print("%.16e" % Ltot.detach().numpy())
