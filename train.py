cf65a21c675d1ee8b674e93c81541cef6cf8a2e1
Sergey Litvinov <slitvinov@gmail.com>
2023-03-08 11:46:36 +0100

    up

--- train.py
+++ train.py
@@ -1,8 +1,14 @@
 import torch
 import math
 
+
 def ini(*shape):
-        return torch.tensor((2 * torch.rand(shape) - 1) / math.sqrt(shape[0]), requires_grad=True)
+	x = torch.empty(shape, requires_grad=True)
+	L = 1 / math.sqrt(shape[0])
+	with torch.no_grad():
+		x.uniform_(-L, L)
+	return x
+
 
 def d2(f, x):
 	df = torch.autograd.grad([f], [x],
@@ -18,10 +24,10 @@ def train():
 	for tt in range(epochs):
 		optimizer.zero_grad()
 		if tt % sc_sampling == 0 and tt < 0.9 * epochs:
-			x = 2 * L * torch.rand(n_train, 1) + L
-			y = 2 * L * torch.rand(n_train, 1) + L
-			z = 2 * L * torch.rand(n_train, 1) + L
-			R = (RxL - RxR) * torch.rand(n_train, 1) + RxR
+			x = 2 * L * torch.rand(n_train, 1) - L
+			y = 2 * L * torch.rand(n_train, 1) - L
+			z = 2 * L * torch.rand(n_train, 1) - L
+			R = (Rhi - Rlo) * torch.rand(n_train, 1) + Rlo
 			r1sq = (x - R)**2 + y**2 + z**2
 			r2sq = (x + R)**2 + y**2 + z**2
 			x[r1sq < cutOff**2] = cutOff
@@ -38,12 +44,13 @@ def train():
 		r2 = torch.sqrt((x + R)**2 + y**2 + z**2)
 		f1 = torch.exp(-r1)
 		f2 = torch.exp(-r2)
-		psi = (
-		    2 * torch.sigmoid(torch.sigmoid(torch.hstack(
-		            (f1, f2)) @ H1a + H1b) @ H2a + H2b) @ H3a + H3b) * (torch.sigmoid(R @ L1a + L1b) @ L2a + L2b) + f1 + f2
-		res = d2(psi, x) + d2(psi, y) + d2(
-		    psi, z) + (torch.sigmoid(torch.sigmoid(R @ E1a + E1b) @ E2a + E2b) @ E3a + E3b +
-		               1 / r1 + 1 / r2) * psi
+		psi = (2 * torch.sigmoid(
+		    torch.sigmoid(torch.hstack(
+		        (f1, f2)) @ H1a + H1b) @ H2a + H2b) @ H3a +
+		       H3b) * (torch.sigmoid(R @ L1a + L1b) @ L2a + L2b) + f1 + f2
+		res = d2(psi, x) + d2(psi, y) + d2(psi, z) + (
+		    torch.sigmoid(torch.sigmoid(R @ E1a + E1b) @ E2a + E2b) @ E3a +
+		    E3b + 1 / r1 + 1 / r2) * psi
 		Ltot = (res**2).mean() + (psi[i1]**2).mean() + (psi[i2]**2).mean()
 		Ltot.backward(retain_graph=False)
 		optimizer.step()
@@ -59,8 +66,8 @@ BCcutoff = 17.5
 cutOff = 0.005
 L = 18
 n_train = 1000
-RxL = 0.2
-RxR = 3
+Rlo = 0.2
+Rhi = 3
 sc_sampling = 1
 h = 16
 e = 32
@@ -82,12 +89,13 @@ L1b = ini(l)
 L2a = ini(l, 1)
 L2b = ini(1)
 
-epochs = 5000
+epochs = 11
 lr = 8e-3
-params = (H1a, H1b, H2a, H2b, H3a, H3b, E1a, E1b, E2a, E2b, E3a, L1a, L1b, L2a, L2b)
+params = (H1a, H1b, H2a, H2b, H3a, H3b, E1a, E1b, E2a, E2b, E3a, L1a, L1b, L2a,
+          L2b)
 train()
 
-epochs = 5000
+epochs = 11
 lr = 1e-4
 params = (E1a, E1b, E2a, E2b, E3a)
 train()
